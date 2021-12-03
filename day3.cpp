5912acdf79bc693daa20b83ca9a3edf94e4bf8a4
Gavin White <white.g@northeastern.edu>
2021-12-03 01:54:35 -0500

    slight modification to day3 solution

--- day3.cpp
+++ day3.cpp
@@ -7,8 +7,8 @@
 void solution(std::ifstream & input) {
   std::string line;
   getline(input, line);
-  std::vector<int>* quants = new std::vector<int>(line.size());
-  for (int i : *quants) {
+  std::vector<int> quants = std::vector<int>(line.size());
+  for (int i : quants) {
     i = 0;
   }
 
@@ -16,16 +16,16 @@ void solution(std::ifstream & input) {
     int i = 0;
     for (char c : line) {
       if (c == '0') {
-        (*quants)[i]--;
+        quants[i]--;
       } else {
-        (*quants)[i]++;
+        quants[i]++;
       }
       ++i;
     }
   }
   std::string bnum = "";
   std::string bnum2 = "";
-  for (int i : *quants) {
+  for (int i : quants) {
     if (i > 0) {
       bnum.append("1");
       bnum2.append("0");
@@ -39,7 +39,6 @@ void solution(std::ifstream & input) {
   unsigned short num2 = std::stoi(bnum2, nullptr, 2);
   
   std::cout << num * num2 << std::endl;
-  delete quants;
 }
 
 int main(int argc, char ** args) {
